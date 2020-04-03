d39658706b0aebddb62218ba94c0b7edd77d6e52
HovhannesYedigaryan <61241149+HovhannesYedigaryan@users.noreply.github.com>
2020-04-03 20:28:20 +0400

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,18 @@
+public class Company {
+    public String companyName;
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Company() {
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
